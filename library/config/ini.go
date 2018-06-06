9e8ee0365696bdf9971d8b1bd605832256d87236
lifuxing <lifuxing@oneniceapp.com>
2018-06-06 18:27:15 +0800

    done

--- library/config/ini.go
+++ library/config/ini.go
@@ -1,9 +1,10 @@
 package config
 
 var (
-	conf_path string = "/home/ldap/lifuxing/fxcodes/gowebchat/conf"
+	conf_path string = "/home/ldap/lifuxing/gopath/src/github.com/jcsz/gowebchat/conf"
 )
 
 func Init() {
 	Parse_log_config()
+	Parse_mysql_config()
 }
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	fmt.Println(config.LogPath)
+	fmt.Println(config.MysqlConf)
 }
 
 func init() {
