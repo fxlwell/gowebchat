e01863f39352e3d7448328d70956efef2040a823
lifuxing <lifuxing@oneniceapp.com>
2018-07-05 21:22:58 +0800

    mysql model

--- library/mysql/model.go
+++ library/mysql/model.go
@@ -1,13 +1,31 @@
 package mysql
 
-import ()
+import (
+	"fmt"
+)
+
+const (
+	MYSQL_NODE_CLUSER_SLAVE  = "slave"
+	MYSQL_NODE_CLUSER_MASTER = "master"
+)
 
 type MysqlModel struct {
-	node  string
-	table string
+	Node   string
+	Table  string
+	Cluser string
 }
 
-func NewMysqlModel(node, table string) (*MysqlModel, error) {
-	m := &MysqlModel{node, table}
-	return m, nil
+func NewMysqlModel(node, table string) *MysqlModel {
+	mo := &MysqlModel{node, table, MYSQL_NODE_CLUSER_SLAVE}
+	return mo
+}
+
+func (md *MysqlModel) GetRows(elem *SqlExpr, result *[]map[string]string) error {
+	sn := fmt.Sprintf("%s-%s,", md.Node, md.Cluser)
+	mysql := Node(sn)
+	if mysql == nil {
+		return fmt.Errorf("there is no mysql sn : %s", sn)
+	}
+	err := mysql.GetRows(md.Table, elem, result)
+	return err
 }
